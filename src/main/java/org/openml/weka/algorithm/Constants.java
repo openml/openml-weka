b42e133e3ac9d4748353737140a219f9c3e6ddd9
janvanrijn <janvanrijn@gmail.com>
2018-06-04 18:11:47 -0400

    added constants file

---
+++ src/main/java/org/openml/weka/algorithm/Constants.java
@@ -0,0 +1,10 @@
+package org.openml.weka.algorithm;
+
+public class Constants {
+
+	public static final int MAX_ALLOWED_PARAMETERS = 61;
+	public static final int MAX_LENGTH_FLOW_NAME = 1024;
+	public static final int MAX_LENGTH_PARAM_NAME = 1024;
+	public static final int MAX_LENGTH_PARAM_VALUE = 2048;
+	
+}
